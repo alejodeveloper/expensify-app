f67e459d31598c696686369df10120e00bc50613
Alejo <alejo.pm92@gmail.com>
2020-07-10 14:50:45 -0500

    [update] fix settings

--- expensify/app/settings.py
+++ expensify/app/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', 'expensify-app'),
         'USER': os.environ.get('DB_USERNAME', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'expensify1234'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'HOST': os.environ.get('DB_HOST', 'postgresql-expensify-app'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
