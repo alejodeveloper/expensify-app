9f8f43a41440a53f6d8f60d2bed4fcb6115413e3
Alejo <alejo.pm92@gmail.com>
2020-07-12 11:34:45 -0500

    [update] add test

--- expensify/users/serializers.py
+++ expensify/users/serializers.py
@@ -5,6 +5,7 @@ Serializers to handle users app
 """
 
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import ExpenseUser
@@ -33,8 +34,21 @@ class ExpenseUserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
+        email = validated_data.get('email')
+        username = validated_data.get('username')
+
+        already_user_email = self.Meta.model.objects.filter(email=email)
+        already_user_username = self.Meta.model.objects.filter(
+            username=username
+        )
+
+        if already_user_email.exists() or already_user_username.exists():
+            error_message = f'User with email {email} or username {username}' \
+                f'already exists'
+            raise ValidationError(error_message)
 
         app_user = self.Meta.model(**validated_data)
+
         if password is not None:
             app_user.set_password(password)
             app_user.save()
--- expensify/users/tests/test_create_expenses_user.py
+++ expensify/users/tests/test_create_expenses_user.py
@@ -34,6 +34,28 @@ class JwtExpenseUserTest(TestCase):
         self.assertIsNotNone(test_request.data.get('access'))
         self.assertIsNotNone(test_request.data.get('refresh'))
 
+    def test_refresh_jwt_tokens(self):
+
+        get_token_data = {
+            "username": "jhon.lenon@test.com",
+            "password": "abcdefg",
+        }
+
+        test_url = reverse('users_urls:token_obtain_pair')
+        test_request = self.test_client.post(test_url, get_token_data)
+
+        refresh_token = test_request.data.get('refresh')
+
+        refresh_data = {
+            "refresh": refresh_token,
+        }
+
+        refresh_token_url = reverse('users_urls:token_refresh')
+
+        test_request = self.test_client.post(refresh_token_url, refresh_data)
+
+        self.assertEqual(test_request.status_code, 200)
+
     def test_unauthorized_get_jwt_tokens(self):
         paul_data = {
             "username": "jhon.lenon@test.com",
@@ -45,3 +67,44 @@ class JwtExpenseUserTest(TestCase):
         self.assertEqual(test_request.status_code, 401)
         self.assertIsNone(test_request.data.get('access'))
         self.assertIsNone(test_request.data.get('refresh'))
+
+    def test_unauthorized_refresh_jwt_tokens(self):
+        refresh_data = {
+            "refresh": "test1234",
+        }
+
+        test_url = reverse('users_urls:token_refresh')
+        test_request = self.test_client.post(test_url, refresh_data)
+        self.assertEqual(test_request.status_code, 401)
+
+    def test_create_user(self):
+        test_create_data_user = {
+            "first_name": "expensify-first",
+            "last_name": "expensify-last_name",
+            "email": "some.email@test.com",
+            "username": "myusername",
+            "password": "expenses1234"
+        }
+
+        test_url = reverse('users_urls:expense_app_user')
+        test_request = self.test_client.post(test_url, test_create_data_user)
+        self.assertEqual(test_request.status_code, 201)
+
+        test_response = test_request.json().get('data')
+
+        self.assertIsNotNone(test_response.get('token'))
+        self.assertIsNotNone(test_response.get('token').get('access'))
+        self.assertIsNotNone(test_response.get('token').get('refresh'))
+
+    def test_create_already_user(self):
+        test_data_user = {
+            "first_name": "expensify-first",
+            "last_name": "expensify-last_name",
+            "email": "jhon.lenon@test.com",
+            "username": "jhon.lenon@test.com",
+            "password": "expenses1234"
+        }
+
+        test_url = reverse('users_urls:expense_app_user')
+        test_request = self.test_client.post(test_url, test_data_user)
+        self.assertEqual(test_request.status_code, 400)
