9c008d47d8eaf4c1813856289efdd7455e25d27e
Alejo <alejo.pm92@gmail.com>
2020-07-12 13:25:51 -0500

    [add] expense models and constants

---
+++ expensify/expenses/constants.py
@@ -0,0 +1,36 @@
+"""
+expenses.constants
+------------------
+Enum Constants for expenses app
+"""
+from enum import Enum
+
+
+class ExpenseTypeSlug(Enum):
+    """
+    Enum class for status for the course log
+    """
+
+    FOOD = 'food'
+    HOSE_SERVICES = 'house-services'
+    TRANSPORTATION = 'transportation'
+    DRINKS_PARTIES = 'drinks-parties'
+    HOBBIES = 'hobbies'
+    SPORTS = 'sports'
+    CLOTHING = 'clothing'
+    FURNITURE = 'FURNITURE'
+    HOME_APPLIANCE = 'home-appliance'
+
+    @classmethod
+    def get_values(cls) -> list:
+        """
+        Return a list with the values of the key from the enumerator
+        """
+        return [key.value for key in cls]
+
+    @classmethod
+    def choices(cls):
+        """
+        Return the choices representation for the model
+        """
+        return [(key.value, key.name) for key in cls]
--- expensify/expenses/models.py
+++ expensify/expenses/models.py
@@ -1,3 +1,49 @@
+import uuid
+
 from django.db import models
 
+from users.models import ExpenseUser
+from .constants import ExpenseTypeSlug
 # Create your models here.
+
+
+class ExpenseType(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=255)
+    slug = models.CharField(
+        blank=False,
+        null=False,
+        choices=ExpenseTypeSlug.choices()
+    )
+
+
+class Expense(models.Model):
+    expense_id = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        null=False,
+        blank=False,
+    )
+    expense_name = models.CharField(blank=True, null=True, max_length=255)
+    expense_type = models.ForeignKey(
+        ExpenseType,
+        related_name="expenses",
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False
+    )
+    cost = models.DecimalField(
+        null=False,
+        blank=False,
+        max_digits=19,
+        decimal_places=2
+    )
+    user = models.ForeignKey(
+        ExpenseUser,
+        related_name="expenses",
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False
+    )
+    date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
